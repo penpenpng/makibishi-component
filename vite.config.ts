26ae769b1aae1bbc8dc3a26f369ba1be9b559fa7
penpenpng <poman1638m@gmail.com>
2024-08-07 01:44:03 +0900

    add pages build mode

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,26 @@ import dts from "vite-plugin-dts";
 
 const entry = (name: string) => path.resolve(import.meta.dirname, "src", name);
 
-export default defineConfig({
-  build: {
-    lib: {
-      name: "makibishi-component",
-      fileName: (_, name) => `${name}.js`,
-      entry: {
-        ["makibishi"]: entry("index.ts"),
-        ["makibishi-component"]: entry("makibishi-component.ts"),
-        ["components/index"]: entry("components/index.ts"),
-      },
-      formats: ["es"],
-    },
-    sourcemap: true,
-  },
-  plugins: [dts()],
+export default defineConfig(({ mode }) => {
+  switch (mode) {
+    case "pages":
+      return {};
+    default:
+      return {
+        build: {
+          lib: {
+            name: "makibishi-component",
+            fileName: (_, name) => `${name}.js`,
+            entry: {
+              ["makibishi"]: entry("index.ts"),
+              ["makibishi-component"]: entry("makibishi-component.ts"),
+              ["components/index"]: entry("components/index.ts"),
+            },
+            formats: ["es"],
+          },
+          sourcemap: true,
+        },
+        plugins: [dts()],
+      };
+  }
 });
